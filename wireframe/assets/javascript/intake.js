72bcdf0aa1155fef5df63cbfb3a8b58651736644
David B. Stocker <coffeemetalcode@gmail.com>
2021-07-27 21:38:27 -0400

    basic wireframe for longarm form

---
+++ wireframe/assets/javascript/intake.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+  console.log('connected');
+
+  /* stitch select modal */
+  $('#job-1-stitch-select').on('click', () => {
+    console.log('select your stitch pattern');
+  });
+});
