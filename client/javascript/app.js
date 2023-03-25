720001c026b7aaae8b90ff71ff5c3cc5dc876a4d
dbstocker <coffeemetalcode@gmail.com>
2023-03-25 17:12:36 -0400

    calculating job price

--- client/javascript/app.js
+++ client/javascript/app.js
@@ -13,54 +13,49 @@ $(document).ready(() => {
     zip: ''
   };
 
+  let job1 = {
+    length: 0,
+    width: 0,
+    stitch: '',
+    thread: '',
+    batting: 0,
+    seams: 0,
+    binding: 0,
+    comment: ''
+  }
+
   // if isLoggedIn
   // populate Personal / Mailing info from customer object
 
   // set all state abbreviations
+  // make this the result of an API request
   const stateAbbreviations = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ]; // 50 united states plus district of columbia
   
   // populate state select box with options
-  // populateSelectBox('state', stateAbbreviations);
+  populateSelectBox('state', stateAbbreviations);
 
-  // set value of customer state
-  $('#state').change(() => {
-    customer.state = $('#state').val();
-    console.log(customer);
+  $('#customer-details').change(() => {
+    customer = updateCustomer();
   });
 
-  // set value of customer name
-  $('#name').change(() => {
-    customer.name = $('#name').val();
-    console.log(customer);
+  $('#job-1-details').change(() => {
+    console.log('job 1 state change');
+    job1 = updateJob();
+    updatePrice(job1.length, job1.width);
   });
-
-  // devise a way to do this dynamically
   
   // populate stitch pattern select dialog
   let stitchPatterns = [];
 
+  // populate thread color select modal
+  let threadColors = [];
+
   $.when(getLongarmOptions()).done((result) => {
     stitchPatterns = result.options.pantographs;
+    threadColors = result.options.threads;
     populateSelectDialog('stitch-select-options', stitchPatterns);
+    populateSelectDialog('thread-select-options', threadColors);
   });
-
-  // abstract function to populate selection modals with options from an input array
-  function populateSelectDialog(dialogId, inputArr) {
-    for (item of inputArr) {
-      $(`#${dialogId}`)
-      .append(`<div class="col-sm-4">
-        <div class="card bg-light mb-4">
-          <div class="card-body">
-            <div class="card-title">${item.name}</div>
-            <img src="${item.imgUrl}" class="card-img-top img-fluid" alt="">
-          </div>
-        </div>
-      </div>`);
-    }
-  }
-
-  // populate thread color select modal
-  const threadColors = [];
 
   // populate batting options select modal
   const battingOptions = [];
@@ -85,8 +80,24 @@ $(document).ready(() => {
 
   // onSubmit -> create and populate confirmation modal
   let order = {};
-
+  
   /* functions */
+
+
+  // abstract function to populate selection modals with options from an input array
+  function populateSelectDialog(dialogId, inputArr) {
+    for (item of inputArr) {
+      $(`#${dialogId}`)
+      .append(`<div class="col-sm-4">
+        <div class="card bg-light mb-4">
+          <div class="card-body">
+            <div class="card-title">${item.name}</div>
+            <img src="${item.imgUrl}" class="card-img-top img-fluid" alt="">
+          </div>
+        </div>
+      </div>`);
+    }
+  }
 
   // abstract function to populate select boxes with options from an input array
   function populateSelectBox(id, inputArr) {
@@ -98,6 +109,42 @@ $(document).ready(() => {
     }
   }
 
+  // update customer information from form
+  function updateCustomer() {
+    let c = {
+      name: $('#name').val(),
+      phone: $('#phone').val(),
+      email: $('#email').val(),
+      address1: $('#address-1').val(),
+      address2: $('#address-2').val(),
+      city: $('#city').val(),
+      state: $('#state').val(),
+      zip: $('#postal').val()
+    };
+    console.log('customer object', c);
+    return c;
+  }
+
+  function updateJob() { 
+    let j = {
+      length: $('#job-1-length').val(),
+      width: $('#job-1-width').val(),
+      stitch: $('#job-1-stitch').val(),
+      thread: $('#job-1-thread').val(),
+    };
+    console.log('job object', j);
+    return j;
+  }
+
+  function updatePrice(length, width) {
+    let area = length * width;
+    let charge = area * 0.025;
+    let price = charge <= 50 ? 50 : charge;
+    let currencyFormattedPrice = `$${price.toFixed(2)}`;
+    $('#job-1-area').attr('value', area);
+    $('#job-1-price').attr('value', currencyFormattedPrice);
+  }
+
   // GET longarm options
   function getLongarmOptions() {
     return $.ajax({
@@ -107,4 +154,5 @@ $(document).ready(() => {
       }
     })
   }
+
 });
