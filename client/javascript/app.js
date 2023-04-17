b23b3979acde3d50f14c66f5e5f4f2a9146faecd
dbstocker <coffeemetalcode@gmail.com>
2023-04-16 20:49:42 -0400

    stalling at dialogs

--- client/javascript/app.js
+++ client/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   console.log('connected');
 
   /* variables */
@@ -10,7 +10,7 @@ $(document).ready(function() {
     address2: '',
     city: '',
     state: '',
-    zip: ''
+    zip: '',
   };
 
   let job1 = {
@@ -21,29 +21,81 @@ $(document).ready(function() {
     batting: 0,
     seams: 0,
     binding: 0,
-    comment: ''
-  }
+    comment: '',
+  };
 
   // if isLoggedIn
   // populate Personal / Mailing info from customer object
 
   // set all state abbreviations
   // make this the result of an API request
-  const stateAbbreviations = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ]; // 50 united states plus district of columbia
-  
+  const stateAbbreviations = [
+    'AL',
+    'AK',
+    'AZ',
+    'AR',
+    'CA',
+    'CO',
+    'CT',
+    'DE',
+    'DC',
+    'FL',
+    'GA',
+    'HI',
+    'ID',
+    'IL',
+    'IN',
+    'IA',
+    'KS',
+    'KY',
+    'LA',
+    'ME',
+    'MD',
+    'MA',
+    'MI',
+    'MN',
+    'MS',
+    'MO',
+    'MT',
+    'NE',
+    'NV',
+    'NH',
+    'NJ',
+    'NM',
+    'NY',
+    'NC',
+    'ND',
+    'OH',
+    'OK',
+    'OR',
+    'PA',
+    'RI',
+    'SC',
+    'SD',
+    'TN',
+    'TX',
+    'UT',
+    'VT',
+    'VA',
+    'WA',
+    'WV',
+    'WI',
+    'WY',
+  ]; // 50 united states plus district of columbia
+
   // populate state select box with options
   populateSelectBox('state', stateAbbreviations);
 
-  $('#customer-details').change(function() {
+  $('#customer-details').change(function () {
     customer = updateCustomer();
   });
 
-  $('#job-1-details').change(function() {
+  $('#job-1-details').change(function () {
     console.log('job 1 state change');
     job1 = updateJob();
     updatePrice(job1.length, job1.width);
   });
-  
+
   // populate stitch pattern select dialog
   let stitchPatterns = [];
 
@@ -80,39 +132,47 @@ $(document).ready(function() {
 
   // onSubmit -> create and populate confirmation modal
   let order = {};
-  
+
   /* functions */
 
   // abstract function to populate selection modals with options from an input array
   function populateSelectDialog(dialogId, inputArr) {
     for (item of inputArr) {
-      $(`#${dialogId}`)
-      .append(`<div class="col-sm-4">
-        <div class="card bg-light mb-4">
-          <div id=${item.id} class="card-body radio">
-            <div class="card-title">${item.name}</div>
-            <img src="${item.imgUrl}" class="card-img-top img-fluid" alt="">
+      $(`#${dialogId}`).append(`<div class='col-sm-4'>
+        <div class='card bg-light mb-4'>
+          <div id=${item.id} class='card-body radio'>
+            <div class='card-title'>${item.name}</div>
+            <img src='${item.imgUrl}' class='card-img-top img-fluid' alt=''>
           </div>
         </div>
       </div>`);
     }
 
-    $('.card-body').click(function() {
+    $('.card-body').click(function () {
       let id = $(this).attr('id');
       console.log(id);
       $('.selected .fa').removeClass('fa-check');
       $('.radio').removeClass('selected');
       $(this).addClass('selected');
     });
+
+    saveDialogSelection('thread-save', 'thread-select-options');
+    saveDialogSelection('stitch-save', 'stitch-select-options');
   }
-  
+
+  function saveDialogSelection(btnId, dialogId) {
+    dialog = $(`#${dialogId}`);
+
+    $(`#${btnId}`).click(function () {
+      console.log(btnId, 'clicked');
+      console.log(dialog);
+    });
+  }
+
   // abstract function to populate select boxes with options from an input array
   function populateSelectBox(id, inputArr) {
     for (item of inputArr) {
-      $(`#${id}`)
-        .append($('<option></option>')
-          .attr('value', item)
-          .text(item));
+      $(`#${id}`).append($('<option></option>').attr('value', item).text(item));
     }
   }
 
@@ -126,13 +186,13 @@ $(document).ready(function() {
       address2: $('#address-2').val(),
       city: $('#city').val(),
       state: $('#state').val(),
-      zip: $('#postal').val()
+      zip: $('#postal').val(),
     };
     console.log('customer object', c);
     return c;
   }
 
-  function updateJob() { 
+  function updateJob() {
     let j = {
       length: $('#job-1-length').val(),
       width: $('#job-1-width').val(),
@@ -158,8 +218,7 @@ $(document).ready(function() {
       url: 'http://localhost:8085/api/longarm/options',
       success: (data) => {
         console.log(data);
-      }
-    })
+      },
+    });
   }
-
 });
