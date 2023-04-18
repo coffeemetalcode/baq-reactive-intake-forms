5579f3150903bb5b9f754e091fe50e817e86584f
dbstocker <coffeemetalcode@gmail.com>
2023-04-18 16:23:00 -0400

    added login component

--- apps/angular-template-driven-forms/src/app/app.module.ts
+++ apps/angular-template-driven-forms/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    FormsModule,
     NgbModule
   ],
   providers: [],
